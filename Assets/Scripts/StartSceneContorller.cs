c01d2db0652a7ef9742ce28c1c1d25b67c587c8e
Esker_Gar <lobachevartem@gmail.com>
2020-06-17 13:38:54 +0300

    FinalStartScene

---
+++ Assets/Scripts/StartSceneContorller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneContorller : MonoBehaviour
+{
+    [SerializeField] private Text bestScore;
+        
+    void Start()
+    {
+        bestScore.GetComponent<ViewScore>().ShowScore(Records.BestScore);
+    }
+    
+}
---
+++ Assets/Scripts/ViewScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewScore : MonoBehaviour
+{
+
+    [SerializeField] private string startString;
+
+    public void ShowScore(float value) => GetComponent<Text>().text = startString + value.ToString();
+}
