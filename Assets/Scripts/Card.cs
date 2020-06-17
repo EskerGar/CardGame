f548855192092d6babedffc7229c14b163cc9648
Esker_Gar <lobachevartem@gmail.com>
2020-06-17 18:58:03 +0300

    Animation

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,19 @@ public class Card : MonoBehaviour
 {
     private GameManager _gameManager;
     private SpriteRenderer _spriteRenderer;
+    private bool _cardFlip = false;
+    private Animator _anim;
     public void Initialize()
     {
         _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
     }
 
     public  void ChangeSprite(Sprite sprite) => _spriteRenderer.sprite = sprite;
+
+    private void OnMouseDown()
+    {
+        _cardFlip = !_cardFlip;
+        _anim.SetBool("MouseDown", _cardFlip);
+    }
 }
